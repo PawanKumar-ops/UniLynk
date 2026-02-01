c358bc2696b4c76fa17f138812b134b3f4d903b3
Anujsharma7027 <anujsharma9466@gmail.com>
2026-02-01 16:39:16 +0530

    bug fix

--- models/user.js
+++ models/user.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+ email: { type: String, unique: true },
+   password: {
+      type: String,
+      required: function () {
+        return this.provider === "credentials";
+      },
+      default: null
+    },
+   image: String,
+
 
 
   name: String,
