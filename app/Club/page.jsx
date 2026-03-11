6d72eaba8f39b38db3d1ba794521cfca2cc16e59
PawanKumar-ops <pawan01200kumar@gmail.com>
2026-03-11 21:53:08 +0530

    Card edited

--- app/Club/page.jsx
+++ app/Club/page.jsx
@@ -84,17 +84,7 @@ const Clubpage = () => {
                                 <div className="whatwedo">
                                     <h1 className="clubprofhead">What We Do</h1>
                                     <div className="whatcardcont">
-                                        <div className="whatcard"><div className="whatcardl"><Sparkle /></div>
-                                            <div className="whatcardr">
-                                                <h1 className="whatcardrh">
-                                                    Hackathon
-                                                </h1>
-                                                <div className="whatcardr-content"><p>
-                                                    content h 
-                                                </p>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <div className="whatcard"></div>
                                         <div className="whatcard"></div>
                                         <div className="whatcard"></div>
                                         <div className="whatcard"></div>
